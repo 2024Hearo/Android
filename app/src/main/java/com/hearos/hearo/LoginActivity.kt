15cfabd7856d5c5b73feeca268c9bbfb56fb92e1
Simmee02 <114573447+Simmee02@users.noreply.github.com>
2023-12-26 04:32:06 +0900

    FEAT
    FEAT Login

--- app/src/main/java/com/hearos/hearo/LoginActivity.kt
+++ app/src/main/java/com/hearos/hearo/LoginActivity.kt
@@ -1,18 +1,82 @@
 package com.hearos.hearo
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.WindowManager
-
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 9001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 상단바 숨기기
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setContentView(R.layout.activity_login)
+
+        // GoogleSignInOptions 구성
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        // GoogleSignInClient 초기화
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Google 로그인 버튼 참조 및 클릭 리스너 설정
+        findViewById<Button>(R.id.btn_login_google).setOnClickListener { signIn() }
     }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Google 로그인 인텐트의 결과 처리
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // 로그인 실패 처리
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // 로그인 성공
+                    Log.d("LoginActivity", "Firebase Auth successful")
+
+                    // 메인 액티비티로 이동하는 코드 (이미 있을 경우 생략)
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish() // 로그인 액티비티 종료
+                } else {
+                    // 로그인 실패
+                    Log.w("LoginActivity", "Firebase Auth failed", task.exception)
+                }
+            }
+    }
+
 }
+
