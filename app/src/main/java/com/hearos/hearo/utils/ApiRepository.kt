9942b140bb5a49f5a5d645caacccc831cd361e87
chaeli <s.kcl5243@gmail.com>
2024-02-25 12:33:36 +0900

    CHORE firestorage 추가, url변경

--- app/src/main/java/com/hearos/hearo/utils/ApiRepository.kt
+++ app/src/main/java/com/hearos/hearo/utils/ApiRepository.kt
@@ -2,7 +2,6 @@ package com.hearos.hearo.utils
 
 class ApiRepository {
     companion object {
-        const val BASE_URL = "https://hearo-17195.uc.r.appspot.com"
-        const val CONTENT_TYPE = "application/json"
+        const val BASE_URL = "http://34.168.33.254:8080"
     }
 }
