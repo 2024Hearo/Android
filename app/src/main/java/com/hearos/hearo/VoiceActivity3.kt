9d0764436490d23b6fe4815429420b97d9b99161
Simmee02 <114573447+Simmee02@users.noreply.github.com>
2024-02-26 09:44:54 +0900

    DESIGN
    ff

--- app/src/main/java/com/hearos/hearo/VoiceActivity3.kt
+++ app/src/main/java/com/hearos/hearo/VoiceActivity3.kt
@@ -1,6 +1,7 @@
 package com.hearos.hearo
 
 import android.app.ProgressDialog
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -62,61 +63,95 @@ class VoiceActivity3 : AppCompatActivity() {
             }
 
             // Firebase Storage 인스턴스 및 참조 생성
-            val storageRef = FirebaseStorage.getInstance().reference.child("string/analysis_results.txt")
+            val storageRef =
+                FirebaseStorage.getInstance().reference.child("string/analysis_results.txt")
 
             // 임시 파일 생성
-            val tempFile = File.createTempFile("analysis_results", ".txt", getExternalFilesDir(null))
+            val tempFile =
+                File.createTempFile("analysis_results", ".txt", getExternalFilesDir(null))
+
+            // 1분 후 실행될 작업 정의
+            val handler = Handler(Looper.getMainLooper())
+            val updateDefaultProgress = Runnable {
+                val defaultResults = listOf(1, 0, 1, 0)
+                val progressBars = listOf(progressBar1, progressBar2, progressBar3, progressBar4)
+                progressBars.forEachIndexed { index, progressBar ->
+                    val value = defaultResults[index]
+                    progressBar.progress = when (value) {
+                        1 -> Math.min(progressBar.max, progressBar.progress + 5)
+                        else -> Math.min(progressBar.max, progressBar.progress + 2)
+                    }
+                }
+                displayImage(R.drawable.bunny)
+                Toast.makeText(this@VoiceActivity3, "Update", Toast.LENGTH_SHORT).show()
+                loadingDialog.dismiss()
+            }
+
+            handler.postDelayed(updateDefaultProgress, 60000)
 
             // Firebase Storage에서 파일 다운로드
             storageRef.getFile(tempFile).addOnSuccessListener {
+                // 성공 시 지연된 작업 취소
+                handler.removeCallbacks(updateDefaultProgress)
+
                 // 파일 다운로드 성공, 임시 파일에서 텍스트 읽기
                 val resultsText = tempFile.readText()
-                // 파일 내용을 줄바꿈으로 분할하여 리스트로 변환
                 val results = resultsText.split("\n").map { it.trim().toInt() }
 
                 val progressBars = listOf(progressBar1, progressBar2, progressBar3, progressBar4)
                 progressBars.forEachIndexed { index, progressBar ->
-                    // 파일에서 읽은 값을 기반으로 ProgressBar 업데이트
                     val value = results.getOrNull(index) ?: 0
                     progressBar.progress = when (value) {
-                        1 -> Math.min(progressBar.max, progressBar.progress + 5) // 값이 1인 경우
-                        else -> Math.min(progressBar.max, progressBar.progress + 2) // 그 외의 경우, 여기서는 0으로 가정
+                        1 -> Math.min(progressBar.max, progressBar.progress + 5)
+                        else -> Math.min(progressBar.max, progressBar.progress + 2)
                     }
                 }
+                displayImage(R.drawable.cat)
 
-                // 로딩 다이얼로그 닫기
                 loadingDialog.dismiss()
-
-                // 작업 완료 후 성공 메시지 표시
-                Toast.makeText(this@VoiceActivity3, "ProgressBar 업데이트 성공!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@VoiceActivity3, "ProgressBar 업데이트 성공!", Toast.LENGTH_SHORT)
+                    .show()
             }.addOnFailureListener { exception ->
-                // 파일 다운로드 실패, 로딩 다이얼로그 닫기
+                // 파일 다운로드 실패 시 지연된 작업 취소
+                handler.removeCallbacks(updateDefaultProgress)
                 loadingDialog.dismiss()
-
-                // 실패 메시지 표시
-                Toast.makeText(this@VoiceActivity3, "파일 다운로드 실패: ${exception.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@VoiceActivity3,
+                    "파일 다운로드 실패: ${exception.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
-
-
     }
+
+    private fun displayImage(drawableId: Int) {
+        val imageView: ImageView = findViewById(R.id.ImageView)
+        imageView.setImageResource(drawableId)
+    }
+
 
     private fun runVoiceScript() {
         RetrofitClient.instance.runVoiceScript().enqueue(object : Callback<VoiceResponse> {
             override fun onResponse(call: Call<VoiceResponse>, response: Response<VoiceResponse>) {
                 if (response.isSuccessful) {
                     Log.i(TAG, "Response successful: ${response.body()?.message}")
-                    Toast.makeText(this@VoiceActivity3, response.body()?.message, Toast.LENGTH_LONG).show()
+                    Toast.makeText(this@VoiceActivity3, response.body()?.message, Toast.LENGTH_LONG)
+                        .show()
                 } else {
                     Log.e(TAG, "Response error: ${response.message()}")
-                    Toast.makeText(this@VoiceActivity3 , "Error: ${response.message()}", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        this@VoiceActivity3,
+                        "Error: ${response.message()}",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
 
             override fun onFailure(call: Call<VoiceResponse>, t: Throwable) {
                 Log.e(TAG, "Network call failed: ${t.message}", t)
-                Toast.makeText(this@VoiceActivity3 , "Failure: ${t.message}", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@VoiceActivity3, "Failure: ${t.message}", Toast.LENGTH_LONG)
+                    .show()
             }
         })
     }
